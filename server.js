debd22259166efe62b62885f67363e6b9781d27a
Adam Hillier <dev@adamhillier.eu>
2017-03-01 17:56:27 +0000

    Serve game from root of domain

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/game.html')
+});
 
 app.listen(8080);
 console.log('Listening on localhost::8080');
