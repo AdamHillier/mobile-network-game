b749df5d509974cfbc6dc27fa176fb7bf31d1159
Joe Crawford <joseph.crawford@univ.ox.ac.uk>
2017-03-11 16:09:28 +0000

    Task 4 done

--- public/game.js
+++ public/game.js
@@ -6,7 +6,7 @@
         numberOfSprites: 20,    // Initial sprite count
         nodeEpsilon: 1,       // Radius in which a sprite has reached a node
         spriteSpeed: 0.005       // Speed of sprites
-    }
+    };
 
     // 'Global' variables
     var canvas;
@@ -20,8 +20,16 @@
     var pendingActions = {
         none: 0,
         placeTower: 1
-    }
+    };
     var currentPendingAction = pendingActions.none;
+		
+		var spriteCallStatus = {
+			none: "#f0f",
+			dialing: "#0ff",
+			dialingPulse: "#00f",
+			success: "#0f0",
+			failure: "#f00"
+		};
     
 
     function initialise() {
@@ -31,13 +39,9 @@
         sprites = new Array();
         towers = new Array();
 
-        // This is currently the only thing we're using JQuery for.
-        // In the future we could in-line the JSON, allowing us to remove
-        // JQuery as a (heavy) dependency.
-        $.getJSON("public/adj_graph.json", function (data) {
-            adjMap = data;
-            initialiseSprites();
-        });
+        // Previously with JQuery, now inline in adj_graph.js
+				adjMap = ADJ_GRAPH;
+				initialiseSprites();
 
         // Initialise roadmap
         roadMap.src = "public/static_map.svg";
@@ -51,14 +55,14 @@
                 var startNode = randomIntBound(numberOfNodes);
                 var neighbours = adjMap["osm_adjacency"][startNode];
                 var randIndx = randomIntBound(neighbours.length);
-                sprites.push(new Sprite(startNode, neighbours[randIndx], getNodePosition(startNode)))
+                sprites.push(new Sprite(startNode, neighbours[randIndx], getNodePosition(startNode)));
             }
         }
 
         // click canvas currently only does one thing: place a tower
         // but this can be extended to add other actions
         canvas.onclick = function(event) {
-            if (currentPendingAction == pendingActions.placeTower){
+            if (currentPendingAction === pendingActions.placeTower){
                 var boundary = canvas.getBoundingClientRect();
                 var x = event.clientX - boundary.left;
                 var y = event.clientY - boundary.top;
@@ -77,7 +81,7 @@
         startTime = timestamp;
 
         // Call update on each sprite
-        sprites.forEach(function (sprite) { sprite.update(elapsed) });
+        sprites.forEach(function (sprite) { sprite.update(elapsed); });
 
         render();
         window.requestAnimationFrame(gameLoop);
@@ -97,9 +101,14 @@
         this.previousNode = prevNode;
         this.targetNode = targetNode;
         this.pos = pos;
+				this.callStatus = spriteCallStatus.none;
     }
     Sprite.prototype.update = function (tFrame) {
-        // move increment
+			  // if sprite is idle, maybe place a call
+				if (this.callStatus === spriteCallStatus.none) { placeCallMaybe(this); }
+				// in call => stationary
+				if (this.callStatus !== spriteCallStatus.none) { tFrame = 0; }
+        // move increment				
         var ds = params.spriteSpeed * tFrame; // how far to move
         var source = getNodePosition(this.previousNode);
         var dest = getNodePosition(this.targetNode);
@@ -128,13 +137,39 @@
             }
             this.targetNode = neighbours[randIndx];
         }
-    }
+				
+				function placeCallMaybe(sprite) {
+					var callProbabilityPerUpdate = 1/3000;
+					if (randomIntBound(1/callProbabilityPerUpdate) < 1) {
+						var callDuration = 5000;
+						var ringRing = setInterval(function () {
+							if (sprite.callStatus === spriteCallStatus.dialing) {
+								sprite.callStatus = spriteCallStatus.dialingPulse;
+							} else {
+								sprite.callStatus = spriteCallStatus.dialing;
+							}
+						}, 50);
+						setTimeout(function () {
+							clearInterval(ringRing);
+							if (randomIntBound(2) < 1) {
+								sprite.callStatus = spriteCallStatus.success;
+							} else {
+								sprite.callStatus = spriteCallStatus.failure;
+							}
+						}, callDuration/3);
+						setTimeout(function () {
+							sprite.callStatus = spriteCallStatus.none;
+						}, callDuration);
+					}
+				}
+				
+    };
     function drawSprites(){
         var radius = 5;
-        context.fillStyle = "#f00";
         sprites.forEach(function (sprite) {
             context.beginPath();
             context.arc(sprite.pos.x, sprite.pos.y, radius, 0, Math.PI * 2, false);
+						context.fillStyle = sprite.callStatus;
             context.fill();
         });
     }
@@ -145,15 +180,15 @@
         this.pos = pos;
     };
     function drawTowers(){
-        context.fillStyle = "blue";
+        context.fillStyle = "cyan";
         towers.forEach(function(tower){
             drawSingleTower(tower);
         });
     }
     function drawSingleTower(tower) {
-        var size = 10;
-        var x = Math.max(tower.pos.x - size/2.0, 0)
-        var y = Math.max(tower.pos.y - size/2.0, 0)
+        var size = 15;
+        var x = Math.max(tower.pos.x - size/2.0, 0);
+        var y = Math.max(tower.pos.y - size/2.0, 0);
         context.fillRect(x, y, size, size);
     };
 
@@ -180,10 +215,8 @@
 
     //another place to use jquery
     //make sure all DOMs are loaded before operating on them
-    $(document).ready(function(){
-
+    document.addEventListener("DOMContentLoaded", function(){
         //bind button actions
-
         var startGameButton = document.getElementById("startGame");
         startGameButton.onclick = function() {
             document.getElementById("placeTower").style.display = "inline"; //show button for placing tower
@@ -195,10 +228,20 @@
             currentPendingAction = pendingActions.placeTower;
             document.getElementById("explanation").style.display = "inline"; //show explanation
         };
+				
+				// Blinking effect on "Sprite dialing..." key
+				setInterval(function () {
+					var style = document.getElementById("dialing").style;
+					if (style.color === "rgb(0, 255, 255)") {
+						style.color = spriteCallStatus.dialingPulse;
+					} else {
+						style.color = spriteCallStatus.dialing;
+					}
+				}, 500);
 
         //initialise game
         initialise();
 
-    })
+    });
     
 })();
