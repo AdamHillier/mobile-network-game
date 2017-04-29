4158bf85add5cd4ad0b859ca7a5731545a117b56
Philippa Hack <soporific.requisitioned@gmail.com>
2017-04-29 16:30:55 +0100

    OK with relative and absolute paths without curves (but does not check that constraint)

--- map-gen/create_adj_map.py
+++ map-gen/create_adj_map.py
@@ -1,33 +1,58 @@
 # From a hand traced map (eg. Inkscape etc.), create the abstract adjacency map that the sprites use to navigate
-# For now, start the python interpreter with `python` and test with `execfile("create_adj_map.py")`
+#  to run in the interpreter: `execfile("create_adj_map.py")`
+import argparse
+
+# Handle command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('infile',
+  help="specify a 450px by 450px svg with some paths in it")
+args = parser.parse_args()
 
 # Import the svg and extract the paths
-# Change the file name from "firstpath.svg" if required
 from xml.dom import minidom
-doc = minidom.parse("firstpath.svg")
-path_strings = [path.getAttribute('d') for path in doc.getElementsByTagName('path')]
+doc = minidom.parse(args.infile)
 
-# Round the string representation of the coordinates to integers, and make everything a tuple
+# Doesn't work yet, but refine to accept the name of the lines layer
+# That way the logical map and the background image can be edited in the same file
+path_strings = [path.getAttribute('d') for path in doc.getElementsByTagName('path')]
+#path_strings = [path.getAttribute("d") for path in group.getElementsByTagName("path") for group in doc.getElementsByTagName("g") if (group.getAttribute("inkscape:label")==u'lines')]
+
+# Round the string representation of the coordinates to integers
 paths = [map(lambda coord: tuple([int(float(c)) for c in coord.split(',')]), path.split()[1:]) for path in path_strings]
 
-# The svg path element has a string representation of the line's start point, then it's relative displacements from that point
-# we need to work with absolute positions, so apply (+start_pos) to all but the first coordinate in the path
+print "Paths before formatting:"
+print paths
+
+# The path data attribute is prefixed by a capital M for absolute paths,
+#   and a lower case m for relative paths
+is_relative_path = map(lambda path: path.split()[0]==u'm', path_strings)
+
 def offset_displacement(p0, p1):
   return (p0[0]+p1[0], p0[1]+p1[1])
 
 absolute_paths = []
-for path in paths:
-  start = path[0]
-  offset = map(lambda p: offset_displacement(start, p), path[1:])
-  offset.insert(0, start)
-  absolute_paths.append(offset)
+for path,is_relative in zip(paths, is_relative_path):
+  if (is_relative):
+    pen_position = path[0]
+    abs_path = [pen_position]
+    for delta in path[1:]:
+      pen_position = offset_displacement(pen_position, delta)
+      abs_path.append(pen_position)
+    absolute_paths.append(abs_path)
+  else:
+    absolute_paths.append(path)
+
+print "Paths after formatting:"
+print absolute_paths
 
 # We need to elimate "duplicates" in the road map, but because the tracing is imperfect, two points designed to be at the same intersection will be some way apart numerically
 
-threshold = 500
+import math
+threshold = 10
 def do_merge(p0, p1):
-  dist_squared = (p0[0] - p1[0])**2 + (p0[1] - p1[1])**2
-  return (dist_squared < threshold)
+  d = math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+  print d
+  return (d < threshold)
 
 class ClosestDict(dict):
   def getClosest(self, point):
@@ -43,8 +68,14 @@ for path in absolute_paths:
     else:
       duplicates[point] = [point]
 
+print "Duplicates dictionary:"
+print duplicates
+
 # Invert the dictionary, so that instead of having one point mapping to all it's duplicates, have every point map to the "true" value
 to_canonical = {point: canonical for canonical,aliases in duplicates.iteritems() for point in aliases}
+
+print "Inverse duplicates dictionary:"
+print to_canonical
 
 # Build the adjacency list: a list of lists, indexed by node number, and containing a list of neighbours (by their node numbers)
 # Node numbers are indicies into a separate list containing the coordinates
@@ -53,10 +84,15 @@ import networkx as nx
 G = nx.Graph()
 for path in absolute_paths:
   for u, v in zip(path, path[1:]):
+    print "adding edge between "+str(u)+" and "+str(v)
     G.add_edge(to_canonical[u], to_canonical[v])
 
+print "Graph G"
+print G.nodes()
+print G.adjacency_list()
+
 def rescale(x, y):
-  # todo
+  # Might not be necessary
   return (x, y)
 
 # renumber the nodes from 0
@@ -64,14 +100,13 @@ coord_to_node = dict((v, i) for i, v in enumerate(G.nodes()))
 adj_list = list(list(coord_to_node[c] for c in adj) for adj in G.adjacency_list())
 rescaled_nodes = map(lambda coord: rescale(*coord), G.nodes())
 
-offset_x, offset_y = map(min, zip(*rescaled_nodes))
-rescaled_offset_nodes = map(lambda (x,y): (x-offset_x, y-offset_y), rescaled_nodes)
-
 # Produce the adjacency graph for game.js
 wrapped = {}
 wrapped['osm_adjacency'] = adj_list
-wrapped['osm_nodes'] = rescaled_offset_nodes
+wrapped['osm_nodes'] = rescaled_nodes
 
-#with open('testing_adj_graph.json', 'w') as fh:
-#  fh.write(json.dumps(wrapped, separators=(',', ':')))
+import json
+with open('../public/adj_graph.js', 'w') as fh:
+  fh.write("ADJ_GRAPH=")
+  fh.write(json.dumps(wrapped, separators=(',', ':')))
 
